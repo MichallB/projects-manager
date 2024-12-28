ad21a3e29a8bf934bb24a72175ffd07295bc14f9
michal <michallburch@gmail.com>
2024-12-28 13:32:16 +0100

    Init resources

---
+++ app/Filament/Resources/ProjectResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProjectResource\Pages;
+use App\Filament\Resources\ProjectResource\RelationManagers;
+use App\Models\Project;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ProjectResource extends Resource
+{
+    protected static ?string $model = Project::class;
+
+    protected static ?string $navigationIcon = "heroicon-o-rectangle-stack";
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make("name")->required(),
+                Forms\Components\TextInput::make("description")->required(),
+                Forms\Components\DatePicker::make("start_date")->required()->default((new DateTimeImmutable())->format(DateTimeInterface::ATOM)),
+                Forms\Components\DatePicker::make("end_date"),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make("name")->searchable()->sortable(),
+                Tables\Columns\TextColumn::make("description"),
+                Tables\Columns\TextColumn::make("start_date")->searchable()->sortable(),
+                Tables\Columns\TextColumn::make("end_date")->searchable()->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\QueryBuilder::make()
+                    ->constraints([
+                        Tables\Filters\QueryBuilder\Constraints\TextConstraint::make("name"),
+                        Tables\Filters\QueryBuilder\Constraints\DateConstraint::make("start_date"),
+                        Tables\Filters\QueryBuilder\Constraints\DateConstraint::make("end_date"),
+                    ]),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RelationManagers\TasksRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            "index" => Pages\ListProjects::route("/"),
+            "create" => Pages\CreateProject::route("/create"),
+            "edit" => Pages\EditProject::route("/{record}/edit"),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ProjectResource/RelationManagers/TasksRelationManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources\ProjectResource\RelationManagers;
+
+use App\Enums\TaskStatus;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class TasksRelationManager extends RelationManager
+{
+    protected static string $relationship = "tasks";
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make("name")->required(),
+                Forms\Components\TextInput::make("description")->required(),
+                Forms\Components\DatePicker::make("start_date")->required()->default((new DateTimeImmutable())->format(DateTimeInterface::ATOM)),
+                Forms\Components\DatePicker::make("end_date"),
+                Forms\Components\ToggleButtons::make("status")
+                    ->inline()
+                    ->required()
+                    ->options(TaskStatus::class),
+                Forms\Components\Select::make("user_id")
+                    ->relationship(name: "user", titleAttribute: "name"),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute("description")
+            ->columns([
+                Tables\Columns\TextColumn::make("name")->searchable()->sortable(),
+                Tables\Columns\TextColumn::make("description")->searchable(),
+                Tables\Columns\TextColumn::make("start_date")->searchable()->sortable(),
+                Tables\Columns\TextColumn::make("end_date")->searchable()->sortable(),
+                Tables\Columns\TextColumn::make("status")->searchable()->sortable(),
+                Tables\Columns\TextColumn::make("user.name")->searchable()->sortable(),
+
+            ])
+            ->filters([
+                Tables\Filters\QueryBuilder::make()
+                    ->constraints([
+                        Tables\Filters\QueryBuilder\Constraints\TextConstraint::make("name"),
+                        Tables\Filters\QueryBuilder\Constraints\DateConstraint::make("start_date"),
+                        Tables\Filters\QueryBuilder\Constraints\DateConstraint::make("end_date"),
+                        Tables\Filters\QueryBuilder\Constraints\SelectConstraint::make("status")
+                            ->multiple()
+                            ->options(TaskStatus::class),
+                    ]),
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
